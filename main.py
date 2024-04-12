38720b76ab9e26b73ba75daa6f96504a0f8af1c4
Marven Trommer <124356252+Marvvvtro-Xtreme@users.noreply.github.com>
2024-04-12 17:52:42 +0200

    main.py aktualisieren

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import random
 
 # initialise all necessarry values
-punkte = 0
-fehler = 0
+points = 0
+misses = 0
 i = 10
 
 # run while loop for game
 while i != 0:
-    zahl1 = random.randint(2,10)
-    zahl2 = random.randint(2,10)
-    print(zahl1,"x",zahl2)
-    richtig1 = (zahl1 * zahl2)
+    number1 = random.randint(2,10)
+    number2 = random.randint(2,10)
+    print(number1,"x",number2)
+    right1 = (number1 * number2)
     
-    loesung = input("Wie lautet die Lösung? (Enter für neue Aufgabe): ")
-    if loesung != "":
-        if zahl1 * zahl2 == loesung:
-            print("Das ist richtig!")
-            punkte = punkte + 1
+    solution = input("How's the solution? (Enter for a new Task): ")
+    if solution != "":
+        if number1 * number2 == solution:
+            print("That's right!")
+            points = points + 1
         else:
-            print("Das ist falsch! Es wäre",richtig1,"gewesen")
-            fehler = fehler + 1
+            print("That's wrong! ",richtig1,"would be right")
+            misses = misses + 1
         i -= 1
 
 # print out result
-print("Du hattest",punkte,"Aufgaben richtig und",fehler,"Fehler gehabt")
+print("You got",points,"Tasks right and",misses,"misses")
